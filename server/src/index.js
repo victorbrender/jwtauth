0a934a3b540325afce053e2227024696c04e760d
Victor Brender <victor.brender@gmail.com>
2020-06-17 07:52:01 +0200

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,6 @@
+require 'dotenv/config';
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { verify } = require('jsonwebtoken');
+const { hash, compare } = require('bcryptjs');
